5648498e08a46b41cb1021c1b95a9da9bb3c52ef
krishnatiwari005 <krishna.08072005@gmail.com>
2026-08-17 00:08:56 +0530

    added ui

---
+++ langgraph_backend.py
@@ -0,0 +1,29 @@
+from langgraph.graph import StateGraph,START,END
+from langchain_groq import ChatGroq
+from typing import TypedDict,Annotated
+from langchain_core.messages import BaseMessage
+from langgraph.graph.message import add_messages
+from langgraph.checkpoint.memory import InMemorySaver
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model=ChatGroq(model="groq/compound")
+
+class ChatbotState(TypedDict):
+    messages:Annotated[list[BaseMessage],add_messages]
+
+def chat_node(state:ChatbotState):
+    messages=state['messages']
+    response=model.invoke(messages)
+    return {'messages':response}
+
+checkpointer=InMemorySaver()
+
+graph=StateGraph(ChatbotState)
+graph.add_node('chat_node',chat_node)
+
+graph.add_edge(START,'chat_node')
+graph.add_edge('chat_node',END)
+
+chat=graph.compile(checkpointer=checkpointer)
